d171ffd0cc236612196f18dd8a53c75b3e35f383
Arnit Gupta <arnit2004@gmail.com>
2023-11-28 17:01:45 +0530

    abcd

--- routes/gsecRoutes.js
+++ routes/gsecRoutes.js
@@ -10,5 +10,4 @@ router.post('/makerequest', authenticate, roleMiddleware(['gsec']), gsecControll
 router.get('/myapprovedrequests', authenticate, roleMiddleware(['gsec']), gsecController.getApprovedRequestsByMe);
 router.get('/mypendingrequests', authenticate, roleMiddleware(['gsec']), gsecController.getPendingRequestsByMe);
 router.get('/myrejectedrequests', authenticate, roleMiddleware(['gsec']), gsecController.getRejectedRequestsByMe);
-router.post('/availablelts', authenticate, roleMiddleware(['gsec']), gsecController.availableLTs)
   module.exports = router;
