8552715a47876e82ffd4d3c46c1e0edd3161e842
aryan011 <guptaaryan380@gmail.com>
2023-11-30 01:25:43 +0530

    fixed system admin

--- routes/systemAdministratorRoutes.js
+++ routes/systemAdministratorRoutes.js
@@ -4,8 +4,9 @@ const roleMiddleware = require('../middlewares/roleBasedMiddlewares')
 const systemAdministratorController = require('../controllers/systemAdministratorController')
 const {authenticate,authorize} = require('../middlewares/authMiddlewares');
 
-router.get('/pendingrequests', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.getPendingRequests);
-router.get('/approvedrequests', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.getApprovedRequests);
+// router.get('/pendingrequests', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.getPendingRequests);
+// router.get('/approvedrequests', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.getApprovedRequests);
+router.get('/allrequests', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.getAllRequest);
 router.put('/reviewed', authenticate, roleMiddleware(['systemAdministrator']), systemAdministratorController.approveOrReject);
 
 
